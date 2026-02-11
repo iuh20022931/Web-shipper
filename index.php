d941a434a71ea11639d394e5a48fef96df9b62ed
iuh20022931 <nhilien1352002@gmail.com>
2026-02-11 13:35:48 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   assets/css/styles.css
    	modified:   assets/images/hero-shipper.png
    	modified:   assets/images/hero.png
    	modified:   assets/images/order.png
    	modified:   assets/images/package.png
    	modified:   index.html
    	modified:   index.php

--- index.php
+++ index.php
@@ -243,6 +243,34 @@ if (isset($_SESSION['user_id'])) {
                     <a href="<?php echo $order_now_link; ?>" class="btn-secondary">Đặt hàng ngay</a>
                     <a href="huong-dan-dat-hang.html" class="btn-secondary btn-blink" target="_blank">📖 Hướng dẫn</a>
                 </div>
+
+                <!-- APP DOWNLOAD BUTTONS -->
+                <div class="app-btns animate-bottom">
+                    <a href="#" class="btn-store">
+                        <span class="store-icon">
+                            <svg viewBox="0 0 384 512">
+                                <path
+                                    d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 79.9c14.2 40.8 32.2 77.7 57.6 77.7 24.4 0 32.4-16.7 61.1-16.7 28.5 0 33.8 16.7 61.1 16.7 25.2 0 46.8-34.9 61.1-76.5 8.7-24.3 14.9-50.6 14.9-50.6s-35.6-11.4-35.4-55.3zM263 103.5c16.1-21.3 26.9-50.6 23.9-81.6-23.1 1.4-51.3 15.2-67.7 36.9-14.5 19-26.8 49.7-23.2 79.3 25.8 2.1 52.2-12.5 67-34.6z" />
+                            </svg>
+                        </span>
+                        <div class="store-text">
+                            <span class="store-subtitle">Download on the</span>
+                            <span class="store-title">App Store</span>
+                        </div>
+                    </a>
+                    <a href="#" class="btn-store">
+                        <span class="store-icon">
+                            <svg viewBox="0 0 512 512">
+                                <path
+                                    d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-37.5 37.5 37.5 37.5 58.9-34.1c13.7-7.5 13.7-20.5 0-28zM104.6 499l220.7-221.3-58.7-58.9L47 512z" />
+                            </svg>
+                        </span>
+                        <div class="store-text">
+                            <span class="store-subtitle">GET IT ON</span>
+                            <span class="store-title">Google Play</span>
+                        </div>
+                    </a>
+                </div>
             </div>
             <div class="hero-image animate-right">
                 <img src="assets/images/hero.png" alt="FastGo Shipper" />
